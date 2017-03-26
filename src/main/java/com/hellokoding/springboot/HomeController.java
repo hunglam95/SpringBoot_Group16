2292e91446be4f0ee336c4f743ce82d8431404b5
hunglam95 <hunglamspk@gmail.com>
2017-03-27 01:00:34 +0700

    First Commit

---
+++ src/main/java/com/hellokoding/springboot/HomeController.java
@@ -0,0 +1,14 @@
+package com.hellokoding.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
